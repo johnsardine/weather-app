29ff240a96e31d84f70b3c4417db1eeb2894a67b
João Sardinha <johnsardine@gmail.com>
2016-07-16 02:06:20 +0100

    Add date formatter filter

--- app/app.module.js
+++ app/app.module.js
@@ -90,6 +90,43 @@ app.factory('SearchService', ['$http', function($http) {
  return SearchService;
 }]);
 
+app.filter('wbDate', ['$filter', function($filter) {
+  return function(owmObject, forAttribute) {
+    owmObject = owmObject || {};
+    timestamp = owmObject.date || Date.now();
+    forAttribute = forAttribute || false;
+
+    var format;
+    var formattedValue;
+    if ( forAttribute ) {
+      format = 'yyyy-MM-dd HH:mm:ss';
+      formattedValue = $filter('date')(timestamp, format);
+    } else {
+      var monthName = $filter('date')(timestamp, 'LLLL');
+      var day = $filter('date')(timestamp, 'd');
+      var ordinalDay = function(day) {
+        var suffix;
+        switch (day) {
+          case 1:
+            suffix = 'st';
+          break;
+          case 2:
+            suffix = 'nd';
+          break;
+          case 3:
+            suffix = 'rd';
+          break;
+          default:
+            suffix = 'th';
+        }
+        return day + suffix;
+      }(parseInt(day));
+      formattedValue = monthName + ' ' + ordinalDay;
+    }
+    return formattedValue;
+  };
+}]);
+
 app.controller('SearchBarController', ['$scope', 'SearchService', function($scope, SearchService) {
 
   // Init form variable
