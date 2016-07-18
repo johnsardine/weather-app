cacd3570b522730ddc47b2fc3758663ca6b7d62c
João Sardinha <johnsardine@gmail.com>
2016-07-19 00:26:58 +0100

    Create name filter

--- app/app.module.js
+++ app/app.module.js
@@ -144,6 +144,20 @@ app.filter('wbTemperature', ['$interpolate', function($interpolate) {
   };
 }]);
 
+app.filter('wbName', ['$interpolate', function($interpolate) {
+  return function(owmObject) {
+    owmObject = owmObject || {};
+
+    var name = '&nbsp;';
+    // Make sure object is set correctly and temperature exists
+    if ( typeof owmObject.name === 'string' ) {
+      name = $interpolate('<span title="{{name}}">{{name}}</span>')({ name: owmObject.name });
+    }
+
+    return name;
+  };
+}]);
+
 app.filter('wbDescription', ['$filter', '$interpolate', function($filter, $interpolate) {
   return function(owmObject) {
     owmObject = owmObject || {};
